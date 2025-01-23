58c936396ebe9a883d45c03c90ae89193a713284
surajbartoula <suraj_bartoula@yahoo.com>
2025-01-23 21:51:55 +0400

    adding

---
+++ C++09/ex00/BitcoinExchane.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchane.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 19:44:55 by sbartoul          #+#    #+#             */
+/*   Updated: 2025/01/23 19:46:29 by sbartoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+class BitcoinExchange {
+private:
+public:
+};
+
+#endif
